b94fc052e37d59d6ae253a25be84ee808e218230
Zac Bowen <zbowen93@gmail.com>
2016-02-27 21:01:45 -0600

    Removed build folders from repository.

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zbb/dev/cpp/HangMan
-# Build directory: /home/zbb/dev/cpp/HangMan/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test1 "runUnitTests")
--- gui_build/CMakeFiles/hm_GUI_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "hm_GUI_automoc.cpp"
-  "CMakeFiles/hm_GUI_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/hm_GUI_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
