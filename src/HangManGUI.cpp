766d76b40f591f6e925c610b6e5df4c854c3add0
Zac Bowen <zbowen93@gmail.com>
2016-04-08 20:33:54 -0500

    Removed duplicate options from top level CMakeLists.txt.

--- src/HangManGUI.cpp
+++ src/HangManGUI.cpp
@@ -3,14 +3,14 @@
 #include <Man.h>
 
 int main(int argc, char** argv) {
-  int WIDTH = 250;
-  int HEIGHT = 150;
+  int WIDTH = 400;
+  int HEIGHT = 250;
   Man mn;
   HangMan hm(mn, 1);
   auto app = Gtk::Application::create(argc, argv,
       "HangMan");
   Gtk::Window window;
-  window.set_default_size(200, 200);
+  window.set_default_size(WIDTH, HEIGHT);
 
   return app -> run(window);
 
